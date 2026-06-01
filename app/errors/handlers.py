ded65d679a8408aba58d9d99e5aa8606c1b2c5b2
alitahakose387-maker <alitahakose387@gmail.com>
2026-06-01 19:48:51 +0300

    403 Hata Sayfası ve  Yetki Erişimi Eklendi

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -8,7 +8,13 @@ def not_found(error):
     return render_template("errors/404.html"), 404
 
 
+@errors_bp.app_errorhandler(403)
+def forbidden_error(error):
+    return render_template("errors/403.html"), 403
+
+
 @errors_bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template("errors/500.html"), 500
+
